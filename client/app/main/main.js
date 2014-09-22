28e68bd1c77e5544b00584b066e4a8bd5355966b
Brian Patterson <brianpatterson2013@gmail.com>
2014-09-22 15:51:06 -0500

    Used generator-angular-fullstack to scaffold out a MEAN stack application

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('3dchessApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
